391017c75cd44cfc784ea2cec8e4e24cb8080c27
Ganesh <Ganesh.Bomble@bluepearlhealthtech.com>
2026-03-18 16:10:01 +0530

    drop down Api and setup minimal Endpoint

---
+++ MajhiPaithani.API/Endpoint/Dropdown.cs
@@ -0,0 +1,36 @@
+﻿using MajhiPaithani.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+namespace MajhiPaithani.API.Endpoint
+{  
+
+   
+        public static class Dropdown
+        {
+            public static void Map(WebApplication app)
+            {
+                var dropdown = app.MapGroup("/api/Dropdown");
+
+                dropdown.MapGet("/", async ([FromQuery] int taskId, DropdownService service) =>
+                {
+                    try
+                    {
+                        var data = await service.GetDropdownListAsync(taskId);
+
+                        return Results.Ok(new
+                        {
+                            StatusCode = StatusCodes.Status200OK,
+                            Message = data.Any() ? "Dropdown data fetched successfully" : "No data found",
+                            Data = data
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        return Results.Problem(detail: $"An error occurred: {ex.Message}");
+                    }
+                })
+                .WithName("GetDropdown")
+                .WithTags("Dropdown");
+            }
+        }
+    
+}
---
+++ MajhiPaithani.Application/DataAccess/DropdownDataAccess.cs
@@ -0,0 +1,48 @@
+﻿using MajhiPaithani.Application.Models.Response;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajhiPaithani.Application.DataAccess
+{
+    public class DropdownDataAccess
+    {
+        private readonly string _connectionString;
+
+        public DropdownDataAccess(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<List<DropdownDto>> GetDropdownListAsync(int taskId)
+        {
+            var list = new List<DropdownDto>();
+
+            using var conn = new SqlConnection(_connectionString);
+            using var cmd = new SqlCommand("getdropdownlit", conn);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@TaskId", taskId);
+
+            await conn.OpenAsync();
+
+            using var reader = await cmd.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                list.Add(new DropdownDto
+                {
+                    Id = reader["Id"] != DBNull.Value ? Convert.ToInt32(reader["Id"]) : 0,
+                    Name = reader["Name"]?.ToString()
+                });
+            }
+
+            return list;
+        }
+    }
+}
