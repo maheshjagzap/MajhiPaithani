f1536937f77bba3baf91ade919cfd2d4228bb792
Ganesh <Ganesh.Bomble@bluepearlhealthtech.com>
2026-03-21 13:30:53 +0530

    upload product immage Api

---
+++ MajhiPaithani.Application/DataAccess/AddproductimagedataAccess.cs
@@ -0,0 +1,70 @@
+﻿using MajhiPaithani.Application.Models.Request;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajhiPaithani.Application.DataAccess
+{
+
+    public class AddproductimagedataAccess
+    {
+        private readonly string _connectionString;
+
+        public AddproductimagedataAccess(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<string> ExecuteProductImagesAsync(List<string> fileUrls, int productid, int userId)
+        {
+            string message = "";
+
+            try
+            {
+                using (var conn = new SqlConnection(_connectionString))
+                using (var cmd = new SqlCommand("AddProductImageData", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    await conn.OpenAsync();
+
+                    foreach (var url in fileUrls)
+                    {
+                        cmd.Parameters.Clear();
+
+                        cmd.Parameters.AddWithValue("@taskid", 1);
+                        cmd.Parameters.AddWithValue("@UserId", userId);
+                        cmd.Parameters.AddWithValue("@iProductId", productid);
+                        cmd.Parameters.AddWithValue("@sImageUrl", url);
+
+                        using var reader = await cmd.ExecuteReaderAsync();
+
+                        if (await reader.ReadAsync())
+                        {
+                            message = reader["Message"]?.ToString();
+                        }
+
+                        reader.Close();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                message = $"SQL Error: {ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                message = $"Error: {ex.Message}";
+            }
+
+            return message;
+        }
+
+
+    }
+}
---
+++ MajhiPaithani.Application/Services/AddProductImageservice.cs
@@ -0,0 +1,27 @@
+﻿
+using MajhiPaithani.Application.DataAccess;
+using MajhiPaithani.Application.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajhiPaithani.Application.Services
+{
+    public class AddProductImageservice
+    {
+        private readonly AddproductimagedataAccess _dataAccess;
+
+        public AddProductImageservice(AddproductimagedataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<string> SaveProductImagesAsync(List<string> fileUrls, int productid, int userId)
+        {
+
+            return await _dataAccess.ExecuteProductImagesAsync(fileUrls, productid,userId);
+        }
+    }
+}
